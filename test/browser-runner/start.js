2a5f01a2e18fca5638f7c509b9a6de91b04e28b7
Andreas Madsen <amwebdk@gmail.com>
2013-01-12 17:21:16 +0100

    [test] add a browser test environment

---
+++ test/browser-runner/start.js
@@ -0,0 +1,50 @@
+
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var blow = require('blow');
+var http = require('http');
+var filed = require('filed');
+var modulebox = require('../../lib/dispatch.js');
+
+//
+// Setup blow server
+//
+var browserDir = path.resolve(__dirname, '..', 'browser');
+var files = fs.readdirSync(browserDir).map(function (basename) {
+  return path.resolve(browserDir, basename);
+});
+
+var indexFile = path.resolve(__dirname, 'index.html');
+
+blow(files, {
+  address: 'localhost',
+  port: 11000,
+  index: indexFile,
+  style: 'bdd'
+});
+
+//
+// Setup moduel server
+//
+
+var box = modulebox({
+  root: path.resolve(__dirname, 'localized'),
+
+  modules: 'modules'
+});
+
+http.createServer(function (req, res) {
+    var href = url.parse(req.url, true);
+
+    if (href.pathname === '/core.js') {
+      req.pipe( filed(box.clientCore) ).pipe(res);
+    } else if (req.pathname === '/module') {
+      req.pipe( box.dispatch(href.query) ).pipe(req);
+    } else {
+      res.statusCode = 404;
+      res.end();
+    }
+}).listen(17000, 'localhost', function () {
+  console.log('module server ready on http://localhost:17000');
+});
---
+++ test/browser/ensure.js
@@ -0,0 +1,8 @@
+
+var assert = chai.assert;
+
+describe('window.modulebox', function () {
+  it('should be a function', function () {
+    assert.equal(typeof window.modulebox, 'function');
+  });
+});
