5d5b9dcbcfc635a70ca2e904998e05e9df113d86
Andreas Madsen <amwebdk@gmail.com>
2013-01-13 17:02:42 +0100

    [test] require.ensure missing module

--- test/browser/ensure.js
+++ test/browser/ensure.js
@@ -1,8 +1,50 @@
 
 var assert = chai.assert;
 
-describe('window.modulebox', function () {
+describe('window.modulebox client constructor', function () {
+
   it('should be a function', function () {
     assert.equal(typeof window.modulebox, 'function');
   });
+
+  it('should throw if no settings was defined', function () {
+    try {
+      var box = window.modulebox();
+    } catch (err) {
+       assert.equal(err.message, 'The settings object must be specified');
+       assert.equal(err.name, 'TypeError');
+    }
+  });
+
+  it('should throw if no url encoder was defined', function () {
+    try {
+      window.modulebox({});
+    } catch (err) {
+       assert.equal(err.message, 'A url method must be provided');
+       assert.equal(err.name, 'TypeError');
+    }
+  });
+
+});
+
+describe('require.ensure method', function () {
+  var box = window.modulebox({
+    url: function (acquired, source, request) {
+      return 'http://localhost:17000/module' +
+        '?acquired=' + JSON.stringify(acquired) +
+        '&source=' + JSON.stringify(source) +
+        '&request=' + JSON.stringify(request);
+    }
+  });
+
+  it('require.ensure returns error if module don\'t exists', function (done) {
+    box.require.ensure('/missing.js', function (err) {
+      assert.equal(err.message, 'Cannot find module \'/missing.js\'');
+      assert.equal(err.name, 'Error');
+      assert.equal(err.code, 'MODULE_NOT_FOUND');
+
+      done(null);
+    });
+  });
+
 });
