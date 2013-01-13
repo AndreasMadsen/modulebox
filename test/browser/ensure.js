2318897d18d89670e0a0c2029a46db8c09234984
Andreas Madsen <amwebdk@gmail.com>
2013-01-13 18:00:38 +0100

    [test] require.ensure double request sucess and failure

--- test/browser/ensure.js
+++ test/browser/ensure.js
@@ -28,8 +28,18 @@ describe('window.modulebox client constructor', function () {
 });
 
 describe('require.ensure method', function () {
+  var send = 0;
+  var acquired = null;
+  var source = null;
+  var request = null;
+
   var box = window.modulebox({
-    url: function (acquired, source, request) {
+    url: function (arg_acquired, arg_source, arg_request) {
+      send += 1;
+      acquired = JSON.parse(JSON.stringify(arg_acquired));
+      request = JSON.parse(JSON.stringify(arg_request));
+      source = JSON.parse(JSON.stringify(arg_source));
+
       return 'http://localhost:17000/module' +
         '?acquired=' + JSON.stringify(acquired) +
         '&source=' + JSON.stringify(source) +
@@ -39,9 +49,49 @@ describe('require.ensure method', function () {
 
   it('require.ensure returns error if module don\'t exists', function (done) {
     box.require.ensure('/missing.js', function (err) {
+      assert.equal(send, 1);
+      assert.deepEqual(acquired, []);
+      assert.deepEqual(source, '/');
+      assert.deepEqual(request, '/missing.js');
+
       assert.equal(err.message, 'Cannot find module \'/missing.js\'');
       assert.equal(err.name, 'Error');
       assert.equal(err.code, 'MODULE_NOT_FOUND');
+
+      done(null);
+    });
+  });
+
+  it('require.ensure don\'t send request twice after failure', function (done) {
+    box.require.ensure('/missing.js', function (err) {
+      assert.equal(send, 1);
+
+      assert.equal(err.message, 'Cannot find module \'/missing.js\'');
+      assert.equal(err.name, 'Error');
+      assert.equal(err.code, 'MODULE_NOT_FOUND');
+
+      done(null);
+    });
+  });
+
+  it('require.ensure returns no error if module was found', function (done) {
+    box.require.ensure('/self_export.js', function (err) {
+      assert.equal(send, 2);
+      assert.deepEqual(acquired, []);
+      assert.deepEqual(source, '/');
+      assert.deepEqual(request, '/self_export.js');
+
+      assert.equal(err, null);
+
+      done(null);
+    });
+  });
+
+  it('require.ensure don\'t send request twice after sucess', function (done) {
+    box.require.ensure('/self_export.js', function (err) {
+      assert.equal(send, 2);
+
+      assert.equal(err, null);
 
       done(null);
     });
