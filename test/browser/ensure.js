937de2eb9428f080f0d4ffb55fd3ad9bbed315ab
Andreas Madsen <amwebdk@gmail.com>
2013-01-13 22:45:08 +0100

    [test] connection and response error handling

--- test/browser/ensure.js
+++ test/browser/ensure.js
@@ -1,7 +1,7 @@
 
 var assert = chai.assert;
 
-describe('require.ensure method', function () {
+describe('module server is a working destination', function () {
   var send = 0;
   var acquired = null;
   var source = null;
@@ -70,9 +70,7 @@ describe('require.ensure method', function () {
       done(null);
     });
   });
-
 });
-
 
 describe('Isolated modulebox', function () {
   var send = 0;
@@ -106,6 +104,68 @@ describe('Isolated modulebox', function () {
       done(null);
     });
   });
-
 });
 
+describe('404 response from request', function () {
+  var send = 0;
+  var acquired = null;
+  var source = null;
+  var request = null;
+
+  var notFoundBox = window.modulebox({
+    url: function (arg_acquired, arg_source, arg_request) {
+      send += 1;
+      acquired = JSON.parse(JSON.stringify(arg_acquired));
+      request = JSON.parse(JSON.stringify(arg_request));
+      source = JSON.parse(JSON.stringify(arg_source));
+
+      return 'http://localhost:17000/wrong';
+    }
+  });
+
+  it('require.ensure it should return an error', function (done) {
+    notFoundBox.require.ensure('/single.js', function (err) {
+      assert.equal(send, 1);
+      assert.deepEqual(acquired, []);
+      assert.deepEqual(source, '/');
+      assert.deepEqual(request, '/single.js');
+
+      assert.equal(err.message, 'Got faulty status code 404');
+      assert.equal(err.name, 'Error');
+
+      done(null);
+    });
+  });
+});
+
+describe('broken response from request', function () {
+  var send = 0;
+  var acquired = null;
+  var source = null;
+  var request = null;
+
+  var brokenBox = window.modulebox({
+    url: function (arg_acquired, arg_source, arg_request) {
+      send += 1;
+      acquired = JSON.parse(JSON.stringify(arg_acquired));
+      request = JSON.parse(JSON.stringify(arg_request));
+      source = JSON.parse(JSON.stringify(arg_source));
+
+      return 'http://localhost:10000';
+    }
+  });
+
+  it('require.ensure it should return an error', function (done) {
+    brokenBox.require.ensure('/single.js', function (err) {
+      assert.equal(send, 1);
+      assert.deepEqual(acquired, []);
+      assert.deepEqual(source, '/');
+      assert.deepEqual(request, '/single.js');
+
+      assert.equal(err.message, 'Could not connect');
+      assert.equal(err.name, 'Error');
+
+      done(null);
+    });
+  });
+});
